fa05ca192bc00dc3cb75cce2d3890e35a02150cb
Eldan Goldenberg <eldang@gmail.com>
2018-06-02 14:46:05 -0700

    housekeeping

--- gen.py
+++ gen.py
@@ -5,7 +5,7 @@ textgen = textgenrnn()
 textgen.reset()
 for i in range(1, 11):
 	textgen.train_from_file("data/tube_stations_with_coords.csv", num_epochs=1)
-	for j in range(1, 5):
-		textgen.generate_to_file('output/iteration-' + str(i) + '-temp-' + str(j/5) + '.csv', n=10, temperature=j/4)
+	for j in range(1, 1+i):
+		textgen.generate_to_file('output/iteration-' + str(i) + '-temp-' + str(j/4) + '.csv', n=10, temperature=j/4)
 
 
