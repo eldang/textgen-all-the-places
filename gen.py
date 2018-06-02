ccad490304e83bd053c58711bdc4c662f62b0dd5
Eldan Goldenberg <eldang@gmail.com>
2018-06-02 14:18:39 -0700

    Simplify: just treating this as a script

--- gen.py
+++ gen.py
@@ -1,21 +1,10 @@
 from textgenrnn import textgenrnn
 
 
-def main():
-	textgen = textgenrnn()
-	textgen.reset()
-	textgen.train_from_file("data/tube_stations_with_coords.csv", num_epochs=5)
-	for i in range(1, 5):
-		textgen.generate_to_file('output/temp-' + str(i/5) + '.csv', n=10, temperature=i/4)
+textgen = textgenrnn()
+textgen.reset()
+textgen.train_from_file("data/tube_stations_with_coords.csv", num_epochs=5)
+for i in range(1, 5):
+	textgen.generate_to_file('output/temp-' + str(i/5) + '.csv', n=10, temperature=i/4)
 
-
-
-if __name__ == "__main__":
-	try:
-		main()
-	except:
-		import sys
-		print(sys.exc_info()[0])
-		import traceback
-		print(traceback.format_exc())
 
