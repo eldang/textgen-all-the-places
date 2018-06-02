73baaf97db854308cea9001f70be6344866e6cc7
Eldan Goldenberg <eldang@gmail.com>
2018-06-02 14:20:20 -0700

    iterates over training and temperature, trying to get a feel for parameters

--- gen.py
+++ gen.py
@@ -3,8 +3,9 @@ from textgenrnn import textgenrnn
 
 textgen = textgenrnn()
 textgen.reset()
-textgen.train_from_file("data/tube_stations_with_coords.csv", num_epochs=5)
-for i in range(1, 5):
-	textgen.generate_to_file('output/temp-' + str(i/5) + '.csv', n=10, temperature=i/4)
+for i in range(1, 11):
+	textgen.train_from_file("data/tube_stations_with_coords.csv", num_epochs=1)
+	for j in range(1, 5):
+		textgen.generate_to_file('output/iteration-' + str(i) + '-temp-' + str(j/5) + '.csv', n=10, temperature=j/4)
 
 
