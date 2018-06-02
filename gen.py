01606def9a23854a9911426671ed28805f52b2dd
Eldan Goldenberg <eldang@gmail.com>
2018-06-01 17:20:33 -0700

    nice quick demo all works now

--- gen.py
+++ gen.py
@@ -6,7 +6,7 @@ def main():
 	textgen.reset()
 	textgen.train_from_file("data/tube_stations_with_coords.csv", num_epochs=5)
 	for i in range(1, 5):
-		textgen.generate_to_file('output/temp-' + str(i/5) + '.csv', n=10, temperature=i/5)
+		textgen.generate_to_file('output/temp-' + str(i/5) + '.csv', n=10, temperature=i/4)
 
 
 
