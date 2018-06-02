d0ca2f884a0c5edd0be2ba788ab3fd12068b32a6
Eldan Goldenberg <eldang@gmail.com>
2018-06-01 17:06:51 -0700

    some tweaks

--- gen.py
+++ gen.py
@@ -3,9 +3,10 @@ from textgenrnn import textgenrnn
 
 def main():
 	textgen = textgenrnn()
+	textgen.reset()
 	textgen.train_from_file("data/tube_stations_with_coords.csv", num_epochs=5)
-	for i in range(0.25, 1, 0.25):
-		textgen.generate_to_file('output/temp-' + str(i) + '.csv', n=10, temperature=i)
+	for i in range(1, 5):
+		textgen.generate_to_file('output/temp-' + str(i/5) + '.csv', n=10, temperature=i/5)
 
 
 
